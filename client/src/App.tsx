f4f3eca3108864bc57aee5b43aa36dfb212c99ad
sreejitaduttaa <sreejitadutta23@gmail.com>
2025-01-31 18:17:14 +0530

    setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,23 @@
-function App() {
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { Box, CssBaseline } from "@mui/material"
+import { BrowserRouter } from "react-router-dom"
 
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
- 
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" pad
+          ding="1rem 2rem 4rem 2rem">
+
+
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
